d9898d46affbc45f6a4ea49336a05cc469598202
Mathieu Bouvet <mathieubouvet42@gmail.com>
2022-09-22 09:54:24 +0000

    Install react navigation

---
+++ screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import styles from "./styles";
+
+const Home = () => {
+  return (
+    <View style={styles.main}>
+      <Text style={styles.title}>Home screen</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  main: {
+    display: "flex",
+    alignItems: "center",
+  },
+  title: {
+    marginTop: 15,
+  },
+});
